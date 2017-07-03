0b9ebdfd4c7daca11bf49e7b147e1b793fdac6e2
Nat! <nat@codeon.de>
2017-07-03 15:00:44 +0200

    improve script

--- install-ubuntu.sh
+++ install-ubuntu.sh
@@ -1,8 +1,14 @@
 #! /bin/sh
 
+if [ ! -z "${TRACE}" ]
+then
+   set -x
+fi
+
 PUBLISHER_PUBLICKEY_URL="http://www.mulle-kybernetik.com/dists/admin-pub.asc"
 PUBLISHER_DEBIAN_URL="http://www.mulle-kybernetik.com"
 PUBLISHER_DOMAIN="mulle-kybernetik.com"
+DEVELOPER_PACKAGE="${DEVELOPER_PACKAGE:-mulle-objc-developer}"
 
 [ -z "${PUBLISHER_PUBLICKEY_URL}" ] && echo "PUBLISHER_PUBLICKEY_URL is missing" >&2 && exit 1
 [ -z "${PUBLISHER_DEBIAN_URL}" ] && echo "PUBLISHER_DEBIAN_URL is missing" >&2 && exit 1
@@ -14,60 +20,93 @@ WGETFLAGS="-q -O -"
 HTTPGET="curl"
 HTTPGETFLAGS="${CURLFLAGS}"
 
-#
-# use wget because it's more lightweight
-#
-if [ -z "`command -v "curl"`" ]
-then
-   HTTPGET="wget"
-   HTTPGETFLAGS="${WGETFLAGS}"
+# todo: use -n an exekutor
 
-   if [ -z "`command -v "wget"`" ]
+fail()
+{
+   echo "${FAIL_EXE} error: $*" >&2
+   exit 1
+}
+
+
+# colorize later or never
+log_verbose()
+{
+   echo "<(|)> $*" >&2
+}
+
+
+main()
+{
+   #
+   # use wget because it's more lightweight
+   #
+   if [ -z "`command -v "curl"`" ]
    then
-      sudo apt-get update ${APTFLAGS}  &&
-      sudo apt-get install ${APTFLAGS} "$@"  wget || exit 1
+      HTTPGET="wget"
+      HTTPGETFLAGS="${WGETFLAGS}"
+
+      if [ -z "`command -v "wget"`" ]
+      then
+         sudo apt-get update ${APTFLAGS} &&
+         sudo apt-get install ${APTFLAGS} "$@" wget || exit 1
+      fi
    fi
-fi
+
+   #
+   # add Codeon debian/ubuntu key and repository
+   #
+   log_verbose "Add Codeon GmbH apt source"
+
+   ${HTTPGET} ${HTTPGETFLAGS} "https://www.codeon.de/dists/nat-codeon.asc" |
+      sudo apt-key add - &&
+      sudo echo "deb [arch=amd64] http://download.codeon.de `lsb_release -c -s` main" \
+       > "/etc/apt/sources.list.d/codeon.de-main.list" || exit 1
+
+   #
+   # add Mulle kybernetiK debian/ubuntu key and repository
+   #
+   log_verbose "Add Mulle kybernetiK apt source"
+
+   ${HTTPGET} ${HTTPGETFLAGS} "${PUBLISHER_PUBLICKEY_URL}" |
+      sudo apt-key add - &&
+      sudo echo "deb [arch=all] ${PUBLISHER_DEBIAN_URL} `lsb_release -c -s` main" \
+         > "/etc/apt/sources.list.d/${PUBLISHER_DOMAIN}-main.list" || exit 1
+
+   #
+   # We need cmake >= 3.0.0 eventually, so add it for older linux versions
+   #
+   case `lsb_release -c -s` in
+      trusty|xenial|wily)
+        #
+        # same as `sudo add-apt-repository ppa:george-edison55/cmake-3.x`, but
+        # doesn't require software-properties-common which adds 40MB for just
+        # these two lines
+        #
+        log_verbose "Install cmake 3.x from george-edison55"
+
+        sudo apt-key adv --keyserver keyserver.ubuntu.com \
+                         --recv-keys  0x084ECFC5828AB726 &&
+        sudo echo "deb http://ppa.launchpad.net/george-edison55/cmake-3.x/ubuntu `lsb_release -c -s` main" \
+           > "/etc/apt/sources.list.d/george-edison55-main.list" || exit 1
+      ;;
+   esac
 
 
-#
-# add Codeon debian/ubuntu key and repository
-#
-${HTTPGET} ${HTTPGETFLAGS} "https://www.codeon.de/dists/nat-codeon.asc" | sudo apt-key add - &&
-sudo echo "deb [arch=amd64] http://download.codeon.de `lsb_release -c -s` main" > /etc/apt/sources.list.d/codeon.de-main.list || exit 1
+   #
+   # Now update and install
+   #
+   sudo apt-get update ${APTFLAGS} &&
+   sudo apt-get install ${APTFLAGS} "$@" "${DEVELOPER_PACKAGE}" || exit 1
 
-#
-# add Mulle kybernetiK debian/ubuntu key and repository
-#
-${HTTPGET} ${HTTPGETFLAGS} "${PUBLISHER_PUBLICKEY_URL}" | sudo apt-key add - &&
-sudo echo "deb [arch=all] ${PUBLISHER_DEBIAN_URL} `lsb_release -c -s` main" > /etc/apt/sources.list.d/${PUBLISHER_DOMAIN}-main.list || exit 1
+   #
+   # Make known as a viable c compiler
+   # Should the priority be above gcc ? Probably not
+   #
+   sudo update-alternatives --install "/usr/bin/cc" \
+                                      "cc" \
+                                      "/usr/bin/mulle-clang" \
+                                      "${CC_PRIORITY:-18}"
+}
 
-#
-# We need cmake >= 3.0.0 eventually, so add it for older linux versions
-#
-case `lsb_release -c -s` in
-   trusty|xenial|wily)
-     #
-     # same as `sudo add-apt-repository ppa:george-edison55/cmake-3.x`, but
-     # doesn't require software-properties-common which adds 40MB for just
-     # these two lines
-     #
-     sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys  0x084ECFC5828AB726 &&
-     sudo echo "deb http://ppa.launchpad.net/george-edison55/cmake-3.x/ubuntu `lsb_release -c -s` main" > /etc/apt/sources.list.d/george-edison55-main.list || exit 1
-   ;;
-esac
-
-
-#
-# Now update and install
-#
-sudo apt-get update ${APTFLAGS} &&
-sudo apt-get install ${APTFLAGS} "$@" "${DEVELOPER_PACKAGE:-mulle-objc-developer}" || exit 1
-
-#
-# Make known as a viable c compiler
-# Should the priority be above gcc ? Probably not
-#
-sudo update-alternatives --install "/usr/bin/cc"  "cc" "/usr/bin/mulle-clang" "${CC_PRIORITY:-18}"
-
-
+main "$@"
